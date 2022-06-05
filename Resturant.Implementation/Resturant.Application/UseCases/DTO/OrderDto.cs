7e2c938038fb45538051d824076ec7656be6d495
bosnic-jovana <106922195+bosnic-jovana@users.noreply.github.com>
2022-06-05 18:02:38 +0200

    Add files via upload

---
+++ Resturant.Implementation/Resturant.Application/UseCases/DTO/OrderDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.Application.UseCases.DTO
+{
+    public class OrderDto : BaseDto
+    {
+        public DateTime Date { get; set; }
+        public IEnumerable<OrderItemDto> OrderItems { get; set; }
+    }
+
+    public class CreateOrderDto
+    {
+        public int? UserId { get; set; }
+        public IEnumerable<OrderItemDto> OrderItems { get; set; }
+    }
+
+    public class OrderItemDto
+    {
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+    }
+}

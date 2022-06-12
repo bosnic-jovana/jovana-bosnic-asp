ab600a1fb61f8e890778d436d46beeba4267f59f
bosnic-jovana <106922195+bosnic-jovana@users.noreply.github.com>
2022-06-12 13:01:02 +0200

    Add files via upload

---
+++ Resturant.DataAccess/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Resturant.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.DataAccess.Configurations
+{
+    public class CategoryConfiguration : EntityConfiguration<Category>
+    {
+        protected override void ConfigureRules(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(50).IsRequired();
+ 
+            builder.HasIndex(x => x.Name).IsUnique();
+
+            builder.HasMany(x => x.Specialities)
+                   .WithOne(x => x.Category)
+                   .HasForeignKey(x => x.CategoryId)
+                   .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}

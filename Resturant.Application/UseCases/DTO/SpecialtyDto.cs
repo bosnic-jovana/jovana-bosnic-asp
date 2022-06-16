e9577a0c6c15a0b72dded749cd7e4e475246fda6
bosnic-jovana <106922195+bosnic-jovana@users.noreply.github.com>
2022-06-16 18:52:45 +0200

    Add files via upload

---
+++ Resturant.Application/UseCases/DTO/SpecialtyDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.Application.UseCases.DTO
+{
+    public class SpecialtyDto : BaseDto
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Weight { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public IEnumerable<string> Ingredients { get; set; }
+
+    }
+
+    public class CreateSpecialtyDto
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Weight { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public IEnumerable<int> IngredientIds { get; set; }
+    }
+}

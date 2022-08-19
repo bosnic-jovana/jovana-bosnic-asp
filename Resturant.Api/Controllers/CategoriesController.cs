515d92c0083368e47161cfa40c722f249ff3da80
bosnic-jovana <106922195+bosnic-jovana@users.noreply.github.com>
2022-08-19 13:07:19 +0200

    Add files via upload

---
+++ Resturant.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Resturant.Application.UseCases.Commands;
+using Resturant.Application.UseCases.DTO;
+using Resturant.Application.UseCases.Queries;
+using Resturant.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Resturant.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CategoriesController : ControllerBase
+    {
+
+        private UseCaseHandler _handler;
+        public CategoriesController(UseCaseHandler handler)
+        {
+            _handler = handler;
+        }
+
+        // GET: api/<CategoriesController>
+        [HttpGet]
+        public IActionResult Get([FromQuery] BaseSearch search, [FromServices] IGetCategoriesQuery query)
+        {
+            return Ok(_handler.HandleQuery(query, search));
+        }
+
+     
+        // POST api/<CategoriesController>
+        [HttpPost]
+        public IActionResult Post([FromForm] CreateBase dto,
+            [FromServices] ICreateCategoryCommand command)
+        {
+            _handler.HandleCommand(command, dto);
+            return StatusCode(201);
+        }
+
+        // PUT api/<CategoriesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] BaseWithNameDto dto, 
+            [FromServices] IEditCategoryCommand command)
+        {
+            dto.Id = id;
+            _handler.HandleCommand(command, dto);
+            return NoContent();
+        }
+    }
+}
---
+++ Resturant.Api/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Resturant.Application.UseCases.Commands;
+using Resturant.Application.UseCases.DTO;
+using Resturant.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Resturant.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly UseCaseHandler _handler;
+        private readonly IRegisterUserCommand _command;
+
+        public UsersController(
+            UseCaseHandler handler,
+            IRegisterUserCommand command)
+        {
+            _handler = handler;
+            _command = command;
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Post([FromBody] UserDto dto)
+        {
+            _handler.HandleCommand(_command, dto);
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+    }
+}

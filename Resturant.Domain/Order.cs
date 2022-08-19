9fd3fd02aea4bd880b556383b1b643c20fb7726f
bosnic-jovana <106922195+bosnic-jovana@users.noreply.github.com>
2022-08-19 13:03:23 +0200

    Add files via upload

---
+++ Resturant.Domain/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.Domain
+{
+    public class Order : Entity
+    {
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}

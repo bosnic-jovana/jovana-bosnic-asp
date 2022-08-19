9fce9315b1ab8ca60cfe8b7386c3b5c0183db968
bosnic-jovana <106922195+bosnic-jovana@users.noreply.github.com>
2022-08-19 13:04:07 +0200

    Add files via upload

---
+++ Resturant.DataAccess/Configurations/OrderConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Resturant.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.DataAccess.Configurations
+{
+    public class OrderConfiguration : EntityConfiguration<Order>
+    {
+        protected override void ConfigureRules(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasMany(x => x.OrderItems)
+                   .WithOne(x => x.Order)
+                   .HasForeignKey(x => x.OrderId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+
+    public class OrderItemConfiguration : EntityConfiguration<OrderItem>
+    {
+        protected override void ConfigureRules(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.Property(x => x.Quantity).HasMaxLength(20).IsRequired();
+        }
+    }
+}
---
+++ Resturant.DataAccess/Configurations/SpecialtyConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Resturant.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.DataAccess.Configurations
+{
+    public class SpecialtyConfiguration : EntityConfiguration<Specialty>
+    {
+        protected override void ConfigureRules(EntityTypeBuilder<Specialty> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Image).HasMaxLength(50).IsRequired();
+            builder.Property(x => x.Weight).HasMaxLength(50).IsRequired();
+
+            builder.HasIndex(x => x.Name).IsUnique();
+
+            builder.HasMany(x => x.SpecialtyIngredients)
+                   .WithOne(x => x.Specialty)
+                   .HasForeignKey(x => x.SpecialtyId)
+                   .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.Pricelists)
+                  .WithOne(x => x.Specialty)
+                  .HasForeignKey(x => x.SpecialtyId)
+                  .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+
+    public class IngredientConfiguration : EntityConfiguration<Ingredient>
+    {
+        protected override void ConfigureRules(EntityTypeBuilder<Ingredient> builder)
+        {
+            builder.Property(x => x.Name).HasMaxLength(50).IsRequired();
+
+            builder.HasIndex(x => x.Name).IsUnique();
+
+            builder.HasMany(x => x.SpecialtyIngredients)
+                   .WithOne(x => x.Ingredient)
+                   .HasForeignKey(x => x.IngredientId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}

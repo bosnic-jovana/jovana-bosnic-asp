001ec59224415904018bf8c36d2c4cfa90c46dad
bosnic-jovana <106922195+bosnic-jovana@users.noreply.github.com>
2022-08-29 23:23:28 +0200

    Add files via upload

---
+++ Resturant.Application/UseCases/Queries/IGetSpecialtiesQuery.cs
@@ -0,0 +1,13 @@
+﻿using Resturant.Application.UseCases.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.Application.UseCases.Queries
+{
+    public interface IGetSpecialtiesQuery : IQuery<BasePagedSearch, PagedResponse<SpecialtyDto>>
+    {
+    }
+}

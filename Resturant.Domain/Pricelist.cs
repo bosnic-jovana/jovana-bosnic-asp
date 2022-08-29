eeee2b7e15335c0fbe592c218e9055b17da6d1ff
bosnic-jovana <106922195+bosnic-jovana@users.noreply.github.com>
2022-08-29 23:21:52 +0200

    Add files via upload

---
+++ Resturant.Domain/Pricelist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.Domain
+{
+    public class Pricelist : Entity
+    {
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+        public int SpecialtyId { get; set; }
+
+        public virtual Specialty Specialty { get; set; }
+        public virtual ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+    }
+}
---
+++ Resturant.Domain/SpecialtyIngredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.Domain
+{
+    public class SpecialtyIngredient
+    {
+        public int SpecialtyId { get; set; }
+        public int IngredientId { get; set; }
+
+        public virtual Specialty Specialty { get; set; }
+        public virtual Ingredient Ingredient { get; set; }
+    }
+}

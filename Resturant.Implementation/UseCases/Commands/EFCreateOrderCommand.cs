47930a493940e8bd0b3e452a84f04e2b81829150
bosnic-jovana <106922195+bosnic-jovana@users.noreply.github.com>
2022-09-01 12:34:10 +0200

    Add files via upload

---
+++ Resturant.Implementation/UseCases/Commands/EFCreateOrderCommand.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using Resturant.Application.UseCases.Commands;
+using Resturant.Application.UseCases.DTO;
+using Resturant.DataAccess;
+using Resturant.Domain;
+using Resturant.Implementation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.Implementation.UseCases.Commands
+{
+    public class EFCreateOrderCommand : EFUseCase, ICreateOrderCommand
+    {
+        private CreateOrderValidator _validator;
+        private IApplicationUser _user;
+        public EFCreateOrderCommand(ResturantDbContext context, CreateOrderValidator validator, IApplicationUser user) : base(context)
+        {
+            _validator = validator;
+            _user = user;
+        }
+
+        public int Id => 3;
+
+        public string Name => "Create order using Entity Framework.";
+
+        public void Execute(CreateOrderDto request)
+        {
+            _validator.ValidateAndThrow(request);
+
+            var order = new Order
+            {
+                UserId = _user.Id
+            };
+
+            var orderItems = request.OrderItems.Select(x => new OrderItem {
+                Quantity = x.Quantity,
+                PricelistId = x.PricelistId,
+                Order = order
+            }).ToList();
+
+            Context.Orders.Add(order);
+            Context.OrderItems.AddRange(orderItems);
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ Resturant.Implementation/UseCases/Commands/EFEditIngredientCommand.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using Resturant.Application.Exceptions;
+using Resturant.Application.UseCases.Commands;
+using Resturant.Application.UseCases.DTO;
+using Resturant.DataAccess;
+using Resturant.Domain;
+using Resturant.Implementation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant.Implementation.UseCases.Commands
+{
+    public class EFEditIngredientCommand : EFUseCase, IEditIngredientCommand
+    {
+        private EditIngredientValidator _validator;
+
+        public EFEditIngredientCommand(ResturantDbContext context, EditIngredientValidator validator) : base(context)
+        {
+            _validator = validator;
+        }
+
+        public int Id => 13;
+
+        public string Name => "Edit ingredient using Entity Framework.";
+
+        public void Execute(BaseWithNameDto request)
+        {
+            var ingredient = Context.Ingredients.Where(x => x.Id == request.Id).FirstOrDefault();
+
+            if (ingredient == null)
+            {
+                throw new EntityNotFoundException(nameof(Ingredient), request.Id);
+            }
+
+            _validator.ValidateAndThrow(request);
+
+            ingredient.Name = request.Name;
+
+            Context.SaveChanges();
+        }
+    }
+}
